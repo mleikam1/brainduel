0c258cb5742f2e273b394a5be1b0b04bd10ddbd9
mleikam1 <53997462+mleikam1@users.noreply.github.com>
2026-01-10 16:11:34 -0600

    Add analytics hooks and error handling for trivia

--- lib/services/analytics_service.dart
+++ lib/services/analytics_service.dart
@@ -1,9 +1,18 @@
+import 'dart:async';
+
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/foundation.dart';
 
 class AnalyticsService {
+  AnalyticsService({FirebaseAnalytics? analytics})
+      : _analytics = analytics ?? FirebaseAnalytics.instance;
+
+  final FirebaseAnalytics _analytics;
+
   void logEvent(String name, {Map<String, Object?> parameters = const {}}) {
     if (kDebugMode) {
       debugPrint('[analytics] $name $parameters');
     }
+    unawaited(_analytics.logEvent(name: name, parameters: parameters));
   }
 }
