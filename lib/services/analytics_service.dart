21d84ba2bbf3a848925af343b9a5b91818dece8a
mleikam1 <53997462+mleikam1@users.noreply.github.com>
2026-01-15 13:25:35 -0600

    Sanitize analytics parameters

--- lib/services/analytics_service.dart
+++ lib/services/analytics_service.dart
@@ -10,9 +10,40 @@ class AnalyticsService {
   final FirebaseAnalytics _analytics;
 
   void logEvent(String name, {Map<String, Object?> parameters = const {}}) {
+    final sanitizedParameters = _sanitizeParameters(parameters);
     if (kDebugMode) {
-      debugPrint('[analytics] $name $parameters');
+      debugPrint('[analytics] $name $sanitizedParameters');
     }
-    unawaited(_analytics.logEvent(name: name, parameters: parameters));
+    unawaited(_analytics.logEvent(name: name, parameters: sanitizedParameters));
+  }
+
+  Map<String, Object> _sanitizeParameters(Map<String, Object?> parameters) {
+    final sanitized = <String, Object>{};
+    parameters.forEach((key, value) {
+      final sanitizedValue = _sanitizeValue(value);
+      if (sanitizedValue != null) {
+        sanitized[key] = sanitizedValue;
+      }
+    });
+    return sanitized;
+  }
+
+  Object? _sanitizeValue(Object? value) {
+    if (value == null) {
+      return null;
+    }
+    if (value is String || value is num) {
+      return value;
+    }
+    if (value is bool) {
+      return value.toString();
+    }
+    if (value is Iterable) {
+      return value.map((entry) => entry?.toString() ?? '').join(',');
+    }
+    if (value is Map) {
+      return value.toString();
+    }
+    return value.toString();
   }
 }
